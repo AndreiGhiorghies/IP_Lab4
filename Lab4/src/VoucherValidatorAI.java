e10ef73999739d232a7e39808ac026fed0d6a608
ingridcalancea <calanceaingridnicole@gmail.com>
2026-03-12 09:27:14 +0200

    Cod cu AI

---
+++ Lab4/src/VoucherValidatorAI.java
@@ -0,0 +1,30 @@
+@Getter
+@Setter
+public class VoucherValidatorAI {
+    private String numeModelML;
+    private double pragRiscAbuz;
+
+    public VoucherValidatorAI(String numeModelML, double pragRiscAbuz) {
+        this.numeModelML = numeModelML;
+        this.pragRiscAbuz = pragRiscAbuz;
+    }
+
+    public boolean valideazaVoucherInteligent(String codVoucher, String adresaIpUser, int conturiCreatePeAcelasiIp) {
+        return false;
+    }
+
+    public void antreneazaModelCuDateNoi(String fisierDateIstoric) {
+    }
+
+    public double calculeazaScorComportamentBot(int clickuriPeSecunda) {
+        return 0.0;
+    }
+
+    public void ajusteazaPragRisc(double noulPrag, boolean esteCampanieActiva) {
+
+    }
+
+    public String genereazaExplicatieRespingere(String codVoucher, String adresaIpUser) {
+        return "";
+    }
+}
